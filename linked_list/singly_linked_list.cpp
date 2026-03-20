401fd6e22ec3d8a83318f6a04f3f60032b57b844
Lazy Sloth <me.niteshpatelsd@gmail.com>
2026-03-21 01:05:28 +0530

    added .gitignore file

--- linked_list/singly_linked_list.cpp
+++ linked_list/singly_linked_list.cpp
@@ -17,6 +17,17 @@ void traverse(Node *head){
   printf("nullptr.\n");
 }
 
+int32_t search(Node *head, const int val){
+  int32_t counter{-1};
+  Node *temp{head};
+  while(temp != nullptr){
+    if(temp->val == val)
+      return ++counter;
+    ++counter;
+  }
+  return counter;
+}
+
 void insert(Node*& head,const int val){
   Node *temp = new Node();
   temp->val = val;
@@ -42,11 +53,16 @@ int main (int argc, char *argv[]) {
   insert(head, 23);
   insert(head, 45);
   insert(head, 65);
-
+  insert(head, 234);
+  insert(head, 23432);
   traverse(head);
+  printf("%i at idx %i\n", 45, search(head, 45));
 
   del(head,45);
   traverse(head);
+  
+  printf("%i at idx %i\n", 45, search(head, 45));
+
 
   printf("Worked Perfectly!\n");
   return 0;
