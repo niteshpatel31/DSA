c4bd69854046117e107a19f560de38a0ca0d7708
Lazy Sloth <me.niteshpatelsd@gmail.com>
2026-04-20 04:26:41 +0530

    dequeue test added

--- queue/double_ended_queue.cpp
+++ queue/double_ended_queue.cpp
@@ -20,6 +20,10 @@ class Dequeue{
             head = mid;
             tail = head+tail;
             std::move(v.begin(), v.end(), v.begin()+mid);
+            this->print();
+            std::cout<<"head -> "<<head<<", tail -> "<<tail<<endl;
+            std::cout<<"size : "<<this->size()<<", vec.size : "<<v.size()<<endl;
+            return;
         }
     public:
         Dequeue(){}
@@ -27,18 +31,15 @@ class Dequeue{
         ~Dequeue(){v.erase(v.begin(), v.end());}
 
         void pushFront(T val){
-            if(v.size() == 0){v.emplace_back(val); head=0; tail=0;}
-            if(head==0){
+            if(v.size() == 0){v.emplace_back(val); head=0; tail=0; return;}
+            if(head==0)
                 this->resize();
-                v[--head] = val;
-            }
+            v[--head] = val;
         }
         void pushBack(T val){
-            if(v.size()==0){v.emplace_back(val); head = 0, tail = 0;}
-            if(tail==v.capacity()-1){
-                this->resize();
-                v[++tail] = val;
-            }
+            if(v.size()==0){v.emplace_back(val); head=0; tail=0;return;}
+            if(tail==v.capacity()-1)this->resize();
+            v[++tail] = val;
         }
         void popFront(){
             if(isEmpty()) throw DEQUEUE_EMPTY;
@@ -50,24 +51,44 @@ class Dequeue{
         }
         T peekFront(){return (isEmpty())?throw DEQUEUE_EMPTY: this->v[head];}
         T peekBack(){return (isEmpty())?throw DEQUEUE_EMPTY: this->v[tail];}
-        bool isEmpty(){return tail-head<0;}
+        bool isEmpty(){return tail<head;}
         size_t size(){return (isEmpty())?0:tail-head+1;}
 
         void print(){
-            for(int i = head; i<= tail; i++)
-                std::cout<<v[i]<<", ";
+            for(auto e : v)
+                std::cout<<e<<", ";
             std::cout<<endl;
+            return;
         }
 };
 
 int main (int argc, char *argv[]) {
-    Dequeue d("something");
+    std::cout<<endl<<"----Numbers----"<<endl;
+    Dequeue<int> num(567);
+    num.pushBack(234);
+    num.pushBack(345);
+    num.pushFront(67);
+    num.pushFront(89);
+    num.pushFront(90);
+    num.popBack();
+
+
+    std::cout<<endl<<"----std::string----"<<endl;
+    Dequeue<std::string> d("something");
     d.pushBack("fuck");
-    d.pushBack("java");
-    d.print();
-    std::cout<<d.size()<<endl;
+    d.pushBack("asdf");
+    d.pushFront("data");
+    d.pushFront("data");
+    d.pushFront("data");
     d.popBack();
-    std::cout<<d.peekFront()<<endl;
-    std::cout<<d.peekBack()<<endl;
+
+    std::cout<<endl<<"----const char*----"<<endl;
+    Dequeue<const char*> c("something");
+    c.pushBack("fuck");
+    c.pushBack("asdf");
+    c.pushFront("data");
+    c.pushFront("data");
+    c.pushFront("data");
+    c.popBack();
     return EXIT_SUCCESS;
 }
