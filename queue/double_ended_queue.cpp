695d74708229f2d6ca839def52d5773c66885f72
Lazy Sloth <me.niteshpatelsd@gmail.com>
2026-04-20 02:49:53 +0530

    double ended queue through linkedlist

--- queue/double_ended_queue.cpp
+++ queue/double_ended_queue.cpp
@@ -1,62 +1,73 @@
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
-#include "../common/node.h"
+#include <utility>
+#include <vector>
 #define endl "\n"
-#define QUEUE_EMPTY std::runtime_error("Queue is Empty!")
+#define DEQUEUE_EMPTY std::runtime_error("Dequeue is Empty!")
 
 template <typename T>
-class Queue{
+class Dequeue{
     private:
-        Node<T> *tail{nullptr};
-        Node<T> *head{nullptr};
-        size_t length{0};
+        std::vector<T> v;
+        int head{-1};
+        int tail{-1};
+        int mid{-1};
 
+        void resize(){
+            v.resize(v.size()*2+v.size()*1.5);
+            mid = v.size()/2;
+            head = mid;
+            tail = head+tail;
+            std::move(v.begin(), v.end(), v.begin()+mid);
+        }
     public:
-        Queue(){}
-        Queue(T val){enqueue(val);}
-        ~Queue(){
-            while(head!=nullptr){
-                Node<T> *tmp = head;
-                head = head->next;
-                delete tmp;
-            }
-        }
+        Dequeue(){}
+        Dequeue(T val): head(0), tail(0){v.emplace_back(val);}
+        ~Dequeue(){v.erase(v.begin(), v.end());}
 
-        void enqueue(T val){
-            Node<T> *tmp = new Node(val);
-            if(tail!=nullptr)
-                tail->next = tmp;
-            else
-                head = tmp;
-            tail = tmp;
-            length++;
-            return;
-        }
-        void dequeue(){
-            if(head!=nullptr){
-                Node<T> *tmp = head;
-                head = head->next;
-                delete tmp;
-                length--;
-                return;
-            }
-            else{
-                length = 0;
+        void pushFront(T val){
+            if(v.size() == 0){v.emplace_back(val); head=0; tail=0;}
+            if(head==0){
+                this->resize();
+                v[--head] = val;
             }
         }
-        T peek(){return (head!=nullptr)?head->val:nullptr;}
-        bool isEmpty(){return length==0;}
-        size_t size(){return this->length;}
+        void pushBack(T val){
+            if(v.size()==0){v.emplace_back(val); head = 0, tail = 0;}
+            if(tail==v.capacity()-1){
+                this->resize();
+                v[++tail] = val;
+            }
+        }
+        void popFront(){
+            if(isEmpty()) throw DEQUEUE_EMPTY;
+            else++head;
+        }
+        void popBack(){
+            if(isEmpty()) throw DEQUEUE_EMPTY;
+            else--tail;
+        }
+        T peekFront(){return (isEmpty())?throw DEQUEUE_EMPTY: this->v[head];}
+        T peekBack(){return (isEmpty())?throw DEQUEUE_EMPTY: this->v[tail];}
+        bool isEmpty(){return tail-head<0;}
+        size_t size(){return (isEmpty())?0:tail-head+1;}
+
+        void print(){
+            for(int i = head; i<= tail; i++)
+                std::cout<<v[i]<<", ";
+            std::cout<<endl;
+        }
 };
 
 int main (int argc, char *argv[]) {
-    Queue queue("Fuck");
-    std::cout<<queue.size()<<endl;
-    queue.enqueue("system");
-    std::cout<<std::boolalpha<<queue.isEmpty()<<endl;
-    queue.dequeue();
-    std::cout<<queue.size()<<endl;
-    queue.dequeue();
-    std::cout<<queue.size()<<endl;
-    std::cout<<std::boolalpha<<queue.isEmpty()<<endl;
-    return 0;
+    Dequeue d("something");
+    d.pushBack("fuck");
+    d.pushBack("java");
+    d.print();
+    std::cout<<d.size()<<endl;
+    d.popBack();
+    std::cout<<d.peekFront()<<endl;
+    std::cout<<d.peekBack()<<endl;
+    return EXIT_SUCCESS;
 }
