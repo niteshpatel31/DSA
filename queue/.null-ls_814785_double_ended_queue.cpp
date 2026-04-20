a4fa95d6e9061c20fdaba7e3d4d4067f4d525b36
Lazy Sloth <me.niteshpatelsd@gmail.com>
2026-04-20 15:50:30 +0530

    dequeue completed

--- queue/.null-ls_814785_double_ended_queue.cpp
+++ queue/.null-ls_814785_double_ended_queue.cpp
@@ -10,48 +10,58 @@ template <typename T>
 class Dequeue{
     private:
         std::vector<T> v;
-        int head{0};
+        int head{-1};
         int tail{-1};
         int mid{-1};
 
         void resize(){
-            std::vector<T> tmp(v.empty() ? 4 : v.size() * 3);
-            mid = (tmp.size() - this->size()) / 2;
-            for (int i = 0; i < this->size() && !isEmpty(); ++i)
-                tmp[mid + i] = std::move(v[head + i]);
-            tail = mid + this->size() - 1;
+            v.resize(v.size()*2+v.size()*1.5);
+            // TODO: instead of inplace std::move move to new temp vec
+            mid = v.size()/2;
             head = mid;
-            v = std::move(tmp);
+            tail = head+tail;
+            std::move(v.begin(), v.end(), v.begin()+mid);
+            this->print();
+            std::cout<<"head -> "<<head<<", tail -> "<<tail<<endl;
+            std::cout<<"size : "<<this->size()<<", vec.size : "<<v.size()<<endl;
+            return;
         }
     public:
         Dequeue(){}
-        Dequeue(T val): head(0), tail(0){v.emplace_back(std::move(val));}
+        Dequeue(T val): head(0), tail(0){v.emplace_back(val);}
         ~Dequeue(){v.erase(v.begin(), v.end());}
 
         void pushFront(T val){
-            if(v.empty() || head == 0) this->resize();
-            v[--head] = std::move(val);
+            if(v.size() == 0){v.emplace_back(val); head=0; tail=0; return;}
+            if(head==0)
+                this->resize();
+            v[--head] = val;
         }
         void pushBack(T val){
-            if(v.empty() || tail == (v.size() - 1)) this->resize();
-            v[++tail] = std::move(val);
+            if(v.size()==0){v.emplace_back(val); head=0; tail=0;\   ``             return;}
+            if(tail==v.capacity()-1)this->resize();
+            v[++tail] = val;
         }
         void popFront(){
             if(isEmpty()) throw DEQUEUE_EMPTY;
-            else ++head;
-            if(isEmpty()) { head = 0; tail = -1; }
+            else++head;
         }
         void popBack(){
             if(isEmpty()) throw DEQUEUE_EMPTY;
-            else --tail;
-            if(isEmpty()) { head = 0; tail = -1; }
+            else--tail;
         }
         T peekFront(){return (isEmpty())?throw DEQUEUE_EMPTY: this->v[head];}
         T peekBack(){return (isEmpty())?throw DEQUEUE_EMPTY: this->v[tail];}
         bool isEmpty(){return tail<head;}
         size_t size(){return (isEmpty())?0:tail-head+1;}
 
-       };
+        void print(){
+            for(auto e : v)
+                std::cout<<e<<", ";
+            std::cout<<endl;
+            return;
+        }
+};
 
 int main (int argc, char *argv[]) {
     std::cout<<endl<<"----Numbers----"<<endl;
@@ -62,29 +72,24 @@ int main (int argc, char *argv[]) {
     num.pushFront(89);
     num.pushFront(90);
     num.popBack();
-    std::cout<<num.peekFront()<<endl;
-    std::cout<<num.peekBack()<<endl;
+
 
     std::cout<<endl<<"----std::string----"<<endl;
     Dequeue<std::string> d("something");
-    d.pushBack("code");
+    d.pushBack("fuck");
     d.pushBack("asdf");
     d.pushFront("data");
-    d.pushFront("fuck");
-    std::cout<<d.peekFront()<<endl;
-    std::cout<<d.peekBack()<<endl;
+    d.pushFront("data");
+    d.pushFront("data");
     d.popBack();
 
     std::cout<<endl<<"----const char*----"<<endl;
     Dequeue<const char*> c("something");
-    c.pushBack("code");
+    c.pushBack("fuck");
     c.pushBack("asdf");
     c.pushFront("data");
-    c.pushFront("fuck");
+    c.pushFront("data");
+    c.pushFront("data");
     c.popBack();
-    std::cout<<c.peekFront()<<endl;
-    std::cout<<c.peekBack()<<endl;
-    d.popBack();
-
     return EXIT_SUCCESS;
 }
